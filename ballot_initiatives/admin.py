de3e5804f6f43351e18551764962c4922f03a17d
Chris Keller <christopherlawrencekeller@gmail.com>
2014-10-03 13:50:45 -0700

    removes tabs in files, replacing for spaces

--- ballot_initiatives/admin.py
+++ ballot_initiatives/admin.py
@@ -3,15 +3,35 @@ from ballot_initiatives.models import Initiative
 
 # Register your models here.
 class InitiativeAdmin(admin.ModelAdmin):
-	fieldsets = [
-		(None, {'fields':['sos_id','ag_id','id_note','title','summary','status','prop_num']}),
-		('Proponent info',{'fields':['proponent','email','phone'],'classes':['collapse']}),
-		('Date info',{'fields':['date_sum_estimate','date_sum','date_circulation_deadline','date_sample_due','date_raw_count_due','date_sample_update','date_qualified','date_failed'],'classes':['collapse']}),
-		('Documents',{'fields':['full_text_link','sig_count_link','fiscal_impact_link']}),
-		('Other/Misc',{'fields': ['election','proposition_type','sigs_req'],'classes':['collapse']}),
-	]
-	list_display = ('sos_id','ag_id','id_note','title','proponent','status','prop_num')
-	search_fields = ['sos_id','ag_id','title','summary','proponent','prop_num']
-	list_filter = ['status']
+    fieldsets = [
+        (None, {'fields':['sos_id','ag_id','id_note','title','summary','status','prop_num']}),
+        ('Proponent info',{'fields':['proponent','email','phone'],'classes':['collapse']}),
+        ('Date info',{'fields':['date_sum_estimate','date_sum','date_circulation_deadline','date_sample_due','date_raw_count_due','date_sample_update','date_qualified','date_failed'],'classes':['collapse']}),
+        ('Documents',{'fields':['full_text_link','sig_count_link','fiscal_impact_link']}),
+        ('Other/Misc',{'fields': ['election','proposition_type','sigs_req'],'classes':['collapse']}),
+    ]
+
+    list_display = (
+        'title',
+        'sos_id',
+        'ag_id',
+        'id_note',
+        'proponent',
+        'status',
+        'prop_num'
+    )
+
+    search_fields = [
+        'sos_id',
+        'ag_id',
+        'title',
+        'summary',
+        'proponent',
+        'prop_num'
+    ]
+
+    list_filter = [
+        'status'
+    ]
 
 admin.site.register(Initiative,InitiativeAdmin)
