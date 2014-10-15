56e023ca2dc975c40260ab80fb1f772c21ea1ec5
Chris Keller <christopherlawrencekeller@gmail.com>
2014-10-15 16:27:07 -0700

    updates to the fab file to use a dev settings variable

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,4 @@
 from __future__ import with_statement
-from django.conf import settings
 import os
 import time
 import datetime
@@ -9,6 +8,9 @@ from fabric.api import local, cd, run, env
 from fabric.contrib.console import confirm
 from fabric.context_managers import lcd
 from fabric.colors import green
+from fabric.contrib import django
+django.settings_module("accountability_tracker.settings_development")
+from django.conf import settings
 
 env.use_ssh_config = True
 
@@ -42,7 +44,7 @@ def move():
     local("python manage.py move_baked_files")
 
 def commit(message='updates'):
-    with lcd("/Volumes/one_tb_hd/_programming/2kpcc/static-projects/test/keller"):
+    with lcd(settings.DEPLOY_DIR):
         try:
             message = raw_input("Enter a git commit message:  ")
             local("git add -A && git commit -m \"%s\"" % message)
