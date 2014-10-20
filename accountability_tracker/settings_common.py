8dbacff83d5d082ef04ba8b8bb2446a6ea8b052b
Chris Keller <christopherlawrencekeller@gmail.com>
2014-10-20 08:40:17 -0700

    adds general email settings to common.py:

--- accountability_tracker/settings_common.py
+++ accountability_tracker/settings_common.py
@@ -21,6 +21,13 @@ SEND_BROKEN_LINK_EMAILS = True
 
 MANAGERS = ADMINS
 
+# Gmail config
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
