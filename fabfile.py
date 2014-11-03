bdf7e13cc50b21bb87b3aa2c208c5a7f4bdac6ae
Chris Keller <christopherlawrencekeller@gmail.com>
2014-11-03 10:31:14 -0800

    adds a couple functions to the fab file

--- fabfile.py
+++ fabfile.py
@@ -31,6 +31,9 @@ def migrate():
     # production function to manually run the scraper in local environment
     local("python manage.py migrate")
 
+def requirements():
+    local("pip install -r requirements.txt")
+
 def data():
     local("python manage.py ingest_contributor_data")
 
