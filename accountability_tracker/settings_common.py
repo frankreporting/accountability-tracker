bad43ccb0abc5d033584f730bd35772d807197b6
Chris Keller <christopherlawrencekeller@gmail.com>
2015-01-12 10:48:18 -0800

    re-factors project settings to use new yml configuration

--- accountability_tracker/settings_common.py
+++ accountability_tracker/settings_common.py
@@ -13,17 +13,17 @@ import os
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 ADMINS = (
-    ('Chris Keller', 'ckeller@scpr.org'),
-    ('Brian Frank', 'brian.frank@scpr.org'),
+    ("Chris Keller", "ckeller@scpr.org"),
+    ("Brian Frank", "brian.frank@scpr.org"),
 )
 
 SEND_BROKEN_LINK_EMAILS = True
 
 MANAGERS = ADMINS
 
-TIME_ZONE = 'America/Los_Angeles'
+TIME_ZONE = "America/Los_Angeles"
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
 SITE_ID = 1
 
@@ -34,70 +34,66 @@ USE_L10N = True
 USE_TZ = True
 
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 )
 
-TEMPLATE_DIRS = (os.path.join(SITE_ROOT, 'templates'),)
+TEMPLATE_DIRS = (os.path.join(SITE_ROOT, "templates"),)
 
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-    #'django.template.loaders.eggs.Loader',
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
+    #"django.template.loaders.eggs.Loader",
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.request',
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.core.context_processors.tz',
-    'django.contrib.messages.context_processors.messages'
+    "django.core.context_processors.request",
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages"
 )
 
 MIDDLEWARE_CLASSES = (
-    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    #'django.middleware.cache.UpdateCacheMiddleware',
-    #'django.middleware.cache.FetchFromCacheMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    #"django.contrib.auth.middleware.SessionAuthenticationMiddleware",
+    #"django.middleware.cache.UpdateCacheMiddleware",
+    #"django.middleware.cache.FetchFromCacheMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.flatpages.middleware.FlatpageFallbackMiddleware",
 )
 
-#CACHE_MIDDLEWARE_ALIAS = 'default'
-#CACHE_MIDDLEWARE_SECONDS = (60 * 5)
-#CACHE_MIDDLEWARE_KEY_PREFIX = ''
-
-ROOT_URLCONF = 'accountability_tracker.urls'
+ROOT_URLCONF = "accountability_tracker.urls"
 
 INSTALLED_APPS = (
-
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django.contrib.redirects',
-    'django.contrib.humanize',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "django.contrib.flatpages",
+    "django.contrib.redirects",
+    "django.contrib.humanize",
 
     # installed applications
-    'maplight_finance',
-    'bakery',
-    #'commander',
+    "maplight_finance",
 
     # api & tools
-    #'django_admin_bootstrapped',
-    #'massadmin',
-    'tastypie',
+    "massadmin",
+    "tastypie",
+    "bakery",
+    "django_admin_bootstrapped.bootstrap3",
+    "django_admin_bootstrapped",
+    "django.contrib.admin",
+    "django.contrib.admindocs",
 )
 
 # A sample logging configuration. The only tangible logging
@@ -107,50 +103,50 @@ INSTALLED_APPS = (
 # more details on how to customize your logging configuration.
 
 LOGGING = {
-    'version': 1,
+    "version": 1,
 
-    'disable_existing_loggers': True,
+    "disable_existing_loggers": True,
 
-    'formatters': {
-        'verbose': {
-            'format' : "\033[1;36m%(levelname)s: %(filename)s (def %(funcName)s %(lineno)s): \033[1;37m %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
+    "formatters": {
+        "verbose": {
+            "format" : "\033[1;36m%(levelname)s: %(filename)s (def %(funcName)s %(lineno)s): \033[1;37m %(message)s",
+            "datefmt" : "%d/%b/%Y %H:%M:%S"
         },
-        'simple': {
-            'format': "\033[1;36m%(levelname)s: %(filename)s (def %(funcName)s %(lineno)s): \033[1;37m %(message)s"
+        "simple": {
+            "format": "\033[1;36m%(levelname)s: %(filename)s (def %(funcName)s %(lineno)s): \033[1;37m %(message)s"
         },
     },
 
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "simple"
         },
 
-        #'file': {
-            #'level': 'DEBUG',
-            #'class': 'logging.FileHandler',
-            #'filename': 'mysite.log',
-            #'formatter': 'verbose'
+        #"file": {
+            #"level": "DEBUG",
+            #"class": "logging.FileHandler",
+            #"filename": "mysite.log",
+            #"formatter": "verbose"
         #},
     },
 
-    'loggers': {
-        'accountability_tracker': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
+    "loggers": {
+        "accountability_tracker": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
         },
     }
 }
 
-TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
+TEST_RUNNER = "django.test.simple.DjangoTestSuiteRunner"
 
-AUTH_PROFILE_MODULE = 'create_user.UserProfile'
+AUTH_PROFILE_MODULE = "create_user.UserProfile"
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Honor the "X-Forwarded-Proto" header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 XS_SHARING_ALLOWED_ORIGINS = "*"
 XS_SHARING_ALLOWED_METHODS = "GET"
--- maplight_finance/management/commands/ingest_contributor_data.py
+++ maplight_finance/management/commands/ingest_contributor_data.py
@@ -15,14 +15,7 @@ import string
 from datetime import tzinfo
 from dateutil import parser
 
-#logger = logging.getLogger("root")
-#logging.basicConfig(
-    #format = "\033[1;36m%(levelname)s: %(filename)s (def %(funcName)s %(lineno)s): \033[1;37m %(message)s",
-    #level=logging.DEBUG
-#)
-
 logger = logging.getLogger("accountability_tracker")
-
 
 local_testing_map_light_csvs = [
     "http://votersedge.org/files/ballot-measures/funding-csv/california-2014-11-prop-1-funding_20140924.csv",
@@ -33,190 +26,35 @@ local_testing_map_light_csvs = [
     "http://votersedge.org/files/ballot-measures/funding-csv/california-2014-11-prop-48-funding_20140924.csv"
 ]
 
+class MapLightApi(object):
 
-def request_map_light_api(MAP_LIGHT_API_KEY):
-    """ make a request to the Map Light API and return JSON data """
     csv_prefix = "http://votersedge.org/files/ballot-measures/funding-csv/"
     url_prefix = "http://votersedge.org/services_open_api/cvg.contest_v1.json?"
-    url_api_key = "apikey=%s" % (MAP_LIGHT_API_KEY)
+    url_api_key = "apikey=%s" % (settings.MAP_LIGHT_API_KEY)
     url_query = "&contest=M193&date=2014-11-02"
     url_request = "%s%s%s" % (url_prefix, url_api_key, url_query)
-    map_light_api_request = requests.get(
-        url_request, headers=settings.MAP_LIGHT_API_HEADERS
-    )
-    if map_light_api_request.status_code == 200:
-        logger.debug("processing json")
-        api_data = map_light_api_request.json()
-        api_data = api_data.items()
-        list_of_maplight_csvs = []
-        for data in api_data:
-            data_as_of_date = data[1]["last_funding_update"]
-            csv_source = data[1]["funding_csv_file"]
-            csv_request = csv_prefix + csv_source
-            data_as_of_date = {
-                "csv_request": csv_request,
-                "data_as_of_date": data_as_of_date
-            }
-            list_of_maplight_csvs.append(data_as_of_date)
-        download_map_light_csv(list_of_maplight_csvs)
-    else:
-        pass
 
 
-def download_map_light_csv(list_of_urls):
-    """ loop through a list of urls, grab the csv file and write each row to a model """
-    for url in list_of_urls:
-        response = urllib2.urlopen(url["csv_request"])
-        file = csv.reader(response, delimiter=',', quoting=csv.QUOTE_ALL)
-        file.next()
-        for column in file:
-            try:
-                transaction = evaluate_transaction_number(column[7], column[0], column[1], column[15], column[11])
-                initiative_instance = Initiative.objects.get(initiative_identifier=str(column[0]))
-                obj, created = InitiativeContributor.objects.get_or_create(
-                    transaction_number = transaction,
-                    defaults={
-                        "initiative_identifier_id": initiative_instance.id,
-                        "stance": str(column[1]),
-                        "transaction_name": str(column[2]),
-                        "committee_id": str(column[3]),
-                        "name": evaluate(str(column[4])),
-                        "name_slug": create_name_slug_from(column[4]),
-                        "employer": str(column[5]),
-                        "occupation": str(column[6]),
-                        "city": str(column[7]),
-                        "state": str(column[8]),
-                        "zip_code": str(column[9]),
-                        "id_number": int(column[10]),
-                        "payment_type": str(column[11]),
-                        "amount": float(column[12]),
-                        "transaction_date": convert_date_to_nicey_format(column[13]),
-                        "filed_date": convert_date_to_nicey_format(column[14]),
-                        "transaction_number": transaction,
-                        "is_individual": str(column[16]),
-                        "donor_type": str(column[17]),
-                        "industry": str(column[18]),
-                        "data_as_of_date": url["data_as_of_date"]
-                    }
-                )
-                if not created:
-                    logger.debug("Record exists")
-                else:
-                    logger.debug("New record created for %s" % (transaction))
-            except Exception, exception:
-                logger.error(exception)
-                break
+    def _init(self, *args, **kwargs):
+
+        """
+        make a request to the Map Light API and return JSON data
+        """
+
+        #logger.debug(self.url_request)
+
+        logger.debug(dir(settings))
 
 
-def import_csv_to_model(csv_file):
-    """ obsolete manager to read a csvfile and write data to model """
-    with open(csv_file, 'r', buffering=0) as imported_file:
-        file = csv.reader(imported_file, delimiter=',', quoting=csv.QUOTE_ALL)
-
-        file.next()
-        for column in file:
-            logger.debug(column)
-            try:
-                transaction = evaluate_transaction_number(column[7], column[0], column[1], column[15], column[11])
-                initiative_instance = Initiative.objects.get(initiative_identifier=str(column[0]))
-                obj, created = InitiativeContributor.objects.get_or_create(
-                    transaction_number = transaction,
-                    defaults={
-                        "initiative_identifier_id": initiative_instance.id,
-                        "stance": str(column[1]),
-                        "transaction_name": str(column[2]),
-                        "committee_id": str(column[3]),
-                        "name": evaluate(str(column[4])),
-                        "name_slug": create_name_slug_from(column[4]),
-                        "employer": str(column[5]),
-                        "occupation": str(column[6]),
-                        "city": str(column[7]),
-                        "state": str(column[8]),
-                        "zip_code": str(column[9]),
-                        #"id_number": column[10],
-                        "payment_type": str(column[11]),
-                        "amount": float(column[12]),
-                        "transaction_date": convert_date_to_nicey_format(column[13]),
-                        "filed_date": convert_date_to_nicey_format(column[14]),
-                        "transaction_number": transaction,
-                        "is_individual": str(column[16]),
-                        "donor_type": str(column[17]),
-                        "industry": str(column[18]),
-                        "data_as_of_date": parser.parse("11/01/2014")
-                    }
-                )
-                if not created:
-                    logger.debug("Record exists")
-                else:
-                    logger.debug("New record created for %s" % (transaction))
-            except Exception, exception:
-                logger.error(exception)
-                break
-    imported_file.close()
 
 
-def evaluate_transaction_number(city, prop_number, stance, transaction_number, payment_type):
-    """ fix if a contribution has no transaction number """
-    if prop_number != "":
-        slug_prop_number = prop_number.lower().replace(". ", "-")
-    else:
-        slug_prop_number = "n-a"
-    if city != "":
-        slug_city = city.lower().replace(" ", "-")
-    else:
-        slug_city = "n-a"
-    if stance != "":
-        slug_stance = stance.lower()
-    else:
-        slug_stance = "n-a"
-    if transaction_number != "":
-        slug_transaction_number = transaction_number.lower().replace(" ", "")
-    else:
-        slug_transaction_number = "n-a"
-    if payment_type != "":
-        slug_payment_type = payment_type.lower().replace(" ", "-")
-    else:
-        slug_payment_type = "n-a"
-    output = "%s-%s-%s-%s-%s" % (slug_prop_number, slug_city, slug_stance, slug_transaction_number, slug_payment_type)
-    return output
 
 
-def evaluate(name):
-    """ if name is empty string, give it a name """
-    if name == "":
-        name = "Unitemized contributions less than 100 dollars"
-    else:
-        name_list = name.split("(")
-        name = name_list[0]
-    return name
-
-
-def create_name_slug_from(name):
-    """ strip punctuation and lowercase donor name to create a common format """
-    if name == "":
-        name = "Unitemized contributions less than 100 dollars"
-    output = str(name).replace("&", "and")
-    output = output.translate(None, string.punctuation)
-    output = output.lower().replace(" ", "-")
-    return output
-
-
-def convert_date_to_nicey_format(date):
-    """ fix for improper dates coming back """
-    if date is None or date is "":
-        returned_date = "1900-01-01"
-    elif date == "0000-00-00":
-        returned_date = "1900-01-01"
-    else:
-        returned_date = parser.parse(date)
-    logger.debug(returned_date)
-    return returned_date
 
 
 class Command(BaseCommand):
-    help = "Imports csv to django model"
+    help = "Begin a request to the MapLight API"
     def handle(self, *args, **options):
-        #request_map_light_api(settings.MAP_LIGHT_API_KEY)
-        #download_map_light_csv(local_testing_map_light_csvs)
-        import_csv_to_model("/Users/KellerUser/Desktop/CA_BM_CF_10_17_pull_final_upload.csv")
-        self.stdout.write("\nScraping finished at %s\n" % str(datetime.datetime.now()))
+        task_run = MapLightApi()
+        task_run._init()
+        self.stdout.write("\nTask finished at %s\n" % str(datetime.datetime.now()))
